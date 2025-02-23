f4bce124d831bc4de55a53a8c6f5b8e9adef7b6e
AbdelazizHachemi <hachemiabdelaziz2028@gmail.com>
2025-02-23 17:57:07 +0100

    added microservices

---
+++ ms-ordonnance/src/main/java/com/example/msordonnance/MsOrdonnanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.msordonnance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsOrdonnanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsOrdonnanceApplication.class, args);
+    }
+
+}
---
+++ ms-ordonnance/src/test/java/com/example/msordonnance/MsOrdonnanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msordonnance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsOrdonnanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
